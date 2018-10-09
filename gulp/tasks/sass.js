a88f9b4f40b575730a45b7959c24b150eed55f87
Anton.f <felseror@gmail.com>
2018-10-09 17:03:53 +0300

    problem

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,12 +1,42 @@
 'use strict';
 
-module.exports = function() {
-	$.gulp.task('sass', function() {
-		return $.gulp.src('./source/style/app.scss')
-			.pipe($.gp.sourcemaps.init())
-			.pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
-			.pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
-			.pipe($.gp.sourcemaps.write())
+module.exports = function() {  
+	const shortConfig = {};
+['border', 'borderRadius', 'color', 'fontSize', 'position', 'size', 'spacing'].forEach((val) => {
+  shortConfig[val] = { skip: '_' };
+}); 
+	$.gulp.task('sass', function() {  
+		const postcss = require('gulp-postcss');  
+		const caralho = require('postcss-caralho');
+		const words = require('curse-words-common');
+		var processors = [ 
+			caralho(words), 
+			require('postcss-short')(shortConfig), 
+			require('rucksack-css')({
+				autoprefixer: false
+			}), 
+  		require("postcss-reporter")({ clearReportedMessages: true }),
+			require('postcss-fixes')(), 
+			require('postcss-hocus'),
+			require('postcss-insert')(), 
+			require('postcss-merge-rules')(), 
+			require('postcss-single-line'),  
+			require('css-declaration-sorter')({
+				order: 'concentric-css'
+			})
+		]
+		return $.gulp.src('./src/style/app.scss')
+			.pipe($.gp.sourcemaps.init()) 
+			.pipe(postcss(processors,
+				{
+					syntax: require('postcss-scss')
+				}
+				
+				))
+			.pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' })) 
+			.pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig })) 
+	
+			.pipe($.gp.sourcemaps.write()) 
 			.pipe($.gulp.dest($.config.root + '/css'))
 			.pipe($.browserSync.stream());
 	})
